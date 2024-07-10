2f19e9f1ef5f24eca1852791814d0a48bb2d386b
otiisaac199 <otichinazaisaac19@gmail.com>
2024-07-10 15:27:48 +0100

    added more styles to my Skill comp

--- src/Components/Skill.jsx
+++ src/Components/Skill.jsx
@@ -1,4 +1,5 @@
 import CompHeading from "./ReuseableComp/CompHeading";
+import ProgressBar from "./ReuseableComp/ProgressBar";
 
 const Skill = () => {
   return (
@@ -7,11 +8,20 @@ const Skill = () => {
       <div className="skill-cont">
         <div className="skill-cont1">
           <h3>Coding Skills</h3>
-          <div></div>
+          <div>
+            <ProgressBar title={"HTML"} percent={"90%"} />
+            <ProgressBar title={"CSS"} percent={"85%"} />
+            <ProgressBar title={"Javascript"} percent={"80%"} />
+            <ProgressBar title={"React"} percent={"75%"} />
+          </div>
         </div>
         <div className="skill-cont2">
           <h3>Professional Skills</h3>
-          <div></div>
+          <div>
+            <ProgressBar title={"Frontend Development"} percent={"82%"} />
+            <ProgressBar title={"Graphic Design"} percent={"70%"} />
+            <ProgressBar title={"Web Design"} percent={"35%"} />
+          </div>
         </div>
       </div>
     </div>
